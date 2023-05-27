c7f5e0fd8bfadd74dc26fb7f4f39306d8ac16d31
MateuszRosner <rosner.mateusz@gmail.com>
2023-05-27 10:49:31 +0200

    k

--- modbus.py
+++ modbus.py
@@ -177,7 +177,7 @@ class Modbus():
             frame.data[3] = 0x00
         
         self.send_frame(frame)
-        self.read_coil_data(dataLen=6)
+        self.read_coil_data(dataLen=5)
 
     def read_ac_params(self, resources):
         frame = ModbusFrame(4)
@@ -217,7 +217,7 @@ class Modbus():
 
         self.send_frame(frame)
 
-        if self.read_coil_data(dataLen=6) == True:
+        if self.read_coil_data(dataLen=5) == True:
             print(f"[INFO] AC state on/off: {self.frame.data[1]}")
             if self.frame.data[1] != int(resources.temp_on):
                 resources.temp_on = bool(self.frame.data[1])
